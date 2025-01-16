7f2702581b006da5f419c76c221bc245f0a99c05
Samuel Åkesson <samak519@student.liu.se>
2025-01-16 03:08:20 +0100

    remove unused

--- main.go
+++ main.go
@@ -4,15 +4,6 @@ import (
 	"github.com/sermuns/schemgo/cmd"
 )
 
-
 func main() {
-	// start := time.Now()
 	cmd.Execute()
-	// inputFilePath, outputFilePath := readInput()
-	// writeSchematic(inputFilePath, outputFilePath)
-	// fmt.Printf(
-	// 	"Parsed `%s` in %s\n",
-	// 	*inputFilePath,
-	// 	time.Since(start),
-	// )
 }
