1fb7852c8bb235fd07c6a8b0350ef84528da1ff1
Samuel Åkesson <samak519@student.liu.se>
2025-01-31 21:54:11 +0100

    Read json array of page contents from stdin, working! This approach is probably the saviour!

--- typst-testing/main.go
+++ typst-testing/main.go
@@ -1,43 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
-	"sync"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	var inp strings.Builder
-	inp.WriteString("#set page(width: auto, height: auto, fill: none)\n")
 
-	args := os.Args
-	if len(args) < 2 {
-		log.Fatal("No input specified.")
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	inp.WriteString(args[1])
-
-	var wg sync.WaitGroup
-	for i := 0; i < 50; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			cmd := exec.Command("typst", "compile", "--format", "svg", "-", "-")
-			cmd.Stdin = strings.NewReader(inp.String())
-			// cmd.Stdout = os.Stdout
-			// cmd.Stderr = os.Stderr
-			err := cmd.Run()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
+	var snippets []string
+	err = json.Unmarshal(data, &snippets)
+	if err != nil {
+		log.Fatal(err)
 	}
-	wg.Wait()
+
+	var cmdIn strings.Builder
+	cmdIn.WriteString("#set page(width:auto,height:auto,margin:0cm,fill:none)\n")
+
+	for _, snippet := range snippets {
+		cmdIn.WriteString(snippet + "\n#pagebreak()\n")
+	}
+
+	os.Mkdir("/dev/shm/schemgo", 0755)
+	cmd := exec.Command("typst", "compile", "--format", "svg", "-", "/dev/shm/schemgo/schemgo{0p}.svg")
+	cmd.Stdin = strings.NewReader(cmdIn.String())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println("Took", time.Since(start))
 }
